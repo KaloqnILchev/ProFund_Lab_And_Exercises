290e335a814d2dfb5d15cbdb5094a538c202b5a4
Kaloqn ILchev <godzeusda@hotmail.com>
2017-10-07 20:52:10 +0300

    Upload solutions for Lab 01 C# Intro and Basic Syntax

---
+++ Labs/Lab01.IntroAndBasicSyntax/L01.IntroAndBasicSyntax/P01.Greeting/StartUP.cs
@@ -0,0 +1,13 @@
+﻿namespace P01.Greeting
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ Labs/Lab01.IntroAndBasicSyntax/L01.IntroAndBasicSyntax/P02.AddTwoNumbers/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace P02.AddTwoNumbers
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int sum = firstNumber + secondNumber;
+
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+        }
+    }
+}
