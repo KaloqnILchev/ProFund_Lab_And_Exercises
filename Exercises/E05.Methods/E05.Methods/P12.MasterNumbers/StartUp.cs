1af83d8fd19b33d0addedc6a2b48df19b08e6ce8
Kaloqn ILchev <godzeusda@hotmail.com>
2017-10-12 00:09:59 +0300

    Uploaded all solutions for exercises 05. Methods

--- Exercises/E05.Methods/E05.Methods/P12.MasterNumbers/StartUp.cs
+++ Exercises/E05.Methods/E05.Methods/P12.MasterNumbers/StartUp.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace P12.MasterNumbers
+﻿namespace P12.MasterNumbers
 {
+    using System;
+
     public class StartUp
     {
         public static void Main(string[] args)
---
+++ Exercises/E05.Methods/E05.Methods/P16.DebuggingExerciseInstructionSet/StartUp.cs
@@ -0,0 +1,51 @@
+﻿namespace P16.DebuggingExerciseInstructionSet
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            string command = Console.ReadLine();
+
+            long result = 0;
+
+            while (command != "END")
+            {
+                string[] codeArgs = command.Split(' ');
+                switch (codeArgs[0])
+                {
+                    case "INC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = ++operandOne;
+                            break;
+                        }
+                    case "DEC":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            result = --operandOne;
+                            break;
+                        }
+                    case "ADD":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = operandOne + operandTwo;
+                            break;
+                        }
+                    case "MLA":
+                        {
+                            long operandOne = long.Parse(codeArgs[1]);
+                            long operandTwo = long.Parse(codeArgs[2]);
+                            result = ((long)operandOne * operandTwo);
+                            break;
+                        }
+                }
+                Console.WriteLine(result);
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
