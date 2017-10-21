7007df60480c44ec567b15602d50026f3b02493b
Kaloqn ILchev <godzeusda@hotmail.com>
2017-10-21 17:43:00 +0300

    new Solutions for Dictionary

--- Exercises/E08.Dictionaries/E08.Dictionaries/P05.HandsOfCards/Startup.cs
+++ Exercises/E08.Dictionaries/E08.Dictionaries/P05.HandsOfCards/Startup.cs
@@ -9,30 +9,46 @@
         public static void Main(string[] args)
         {
             string input;
-            Dictionary<string, List<string>> players = new Dictionary<string, List<string>>();
+            Dictionary<string, HashSet<int>> players = new Dictionary<string, HashSet<int>>();
 
-            while ((input = Console.ReadLine()) != "Joker")
-            {
-                string[] playersEnters = input.Split(':').ToArray();
-                string playerName = playersEnters[0];
-                string playerHand = playersEnters[1];
-                string powerOfCard = "";
-                string typeOfCard = "";
-
-                Dictionary<string, int> cardsType = new Dictionary<string, int>()
+            Dictionary<string, int> cardPowers = new Dictionary<string, int>()
                 {
-                    2 2;
+                    { "2", 2}, { "3", 3}, { "4", 4}, { "5", 5}, { "6", 6},
+                    { "7", 7}, { "8", 8}, { "9", 9}, { "10", 10}, { "J", 11},
+                    { "Q", 12}, { "K", 13}, { "A", 14}
+
+                };
+            Dictionary<string, int> cardTypes = new Dictionary<string, int>()
+                {
+                    { "S", 4}, { "H", 3}, { "D", 2}, { "C", 1}
                 };
 
-                string[] playerCards = playerHand.Split(new char[] { ',', ' ' },
-                    StringSplitOptions.RemoveEmptyEntries).ToArray();
-
+            while ((input = Console.ReadLine()) != "JOKER")
+            {
+                var playersEnters = input.Split(':');
+                var playerName = playersEnters[0];
+                var playerCards = playersEnters[1].Split(", ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
 
                 foreach (var card in playerCards)
                 {
+                    var cardPower = card.Substring(0, card.Length - 1);
+                    var cardType = card.Substring(card.Length - 1);
 
-                }
+                    var sum = cardPowers[cardPower] * cardTypes[cardType];
 
+                    if (!players.ContainsKey(playerName))
+                    {
+                        players[playerName] = new HashSet<int>();
+                    }
+                    players[playerName].Add(sum);
+                }               
+            }
+            foreach (var person in players)
+            {
+                var name = person.Key;
+                var cardsSum = person.Value.Sum();
+
+                Console.WriteLine($"{name}: {cardsSum}");
             }
         }
     }
