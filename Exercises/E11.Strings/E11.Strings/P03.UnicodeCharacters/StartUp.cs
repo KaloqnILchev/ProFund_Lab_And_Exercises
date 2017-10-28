995e99b08cd59366e6349190c243188688b34042
Kaloqn ILchev <godzeusda@hotmail.com>
2017-10-28 20:32:32 +0300

    new upload

---
+++ Exercises/E11.Strings/E11.Strings/P03.UnicodeCharacters/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace P03.UnicodeCharacters
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetUnicodeString(input));                     
+
+        }
+        static string GetUnicodeString(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char cha in str)
+            {
+                sb.Append("\\u");
+                sb.Append(String.Format("{0:x4}", (int)cha));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Exercises/E11.Strings/E11.Strings/P04.CharacterMultiplier/StartUp.cs
@@ -0,0 +1,53 @@
+﻿namespace P04.CharacterMultiplier
+{
+    using System;
+    using System.Linq;
+    
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+            char[] firstWord = input[0].ToCharArray();
+            char[] secondWord = input[1].ToCharArray();
+            
+            int Sum = 0;
+
+            if (firstWord.Length == secondWord.Length)
+            {
+
+                for (int i = 0; i < firstWord.Length; i++)
+                {
+                    
+                    Sum += firstWord[i] * secondWord[i];
+                }
+                Console.WriteLine(Sum);
+            }
+            else if (firstWord.Length > secondWord.Length)
+            {
+                for (int i = 0; i < secondWord.Length; i++)
+                {
+                    Sum += firstWord[i] * secondWord[i];
+                }
+                for (int i = secondWord.Length; i < firstWord.Length; i++)
+                {
+                    Sum += firstWord[i];
+                }
+                Console.WriteLine(Sum);
+            }
+            else
+            {
+                for (int i = 0; i < firstWord.Length; i++)
+                {
+                    Sum += firstWord[i] * secondWord[i];
+                }
+                for (int i = firstWord.Length; i < secondWord.Length; i++)
+                {
+                    Sum += secondWord[i];
+                }
+                Console.WriteLine(Sum);
+            }
+        }
+    }
+}
