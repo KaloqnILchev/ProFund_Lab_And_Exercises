c754e2554d2e4af07db84b11fa4c124440a004a4
Kaloqn ILchev <godzeusda@hotmail.com>
2017-10-30 13:27:37 +0200

    upload

---
+++ Labs/Lab11.Strings/Lab11.Strings/E00.Examples01/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P00.Examples01
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine().ToLower();
+            string pattern = Console.ReadLine().ToLower();
+
+            int counter = 0;
+
+            int index = inputText.IndexOf(pattern);
+
+            while (index != -1)
+            {
+                counter++;
+                index = inputText.IndexOf(pattern, index + 1);
+            }
+
+            Console.WriteLine(counter);
+
+        }
+    }
+}
