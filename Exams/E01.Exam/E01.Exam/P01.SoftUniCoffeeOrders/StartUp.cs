3f8db47180309803ccf473af4dbcccb97c75914d
Kaloqn ILchev <godzeusda@hotmail.com>
2017-10-30 14:49:20 +0200

    Exam 01 problems

---
+++ Exams/E01.Exam/E01.Exam/P01.SoftUniCoffeeOrders/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace P01.SoftUniCoffeeOrders
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            decimal totalPrice = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                decimal price = ReadOrderAndCalculatePrice();
+                totalPrice += price;
+
+            }
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+
+        private static decimal ReadOrderAndCalculatePrice()
+        {
+            decimal pricePerCapsule = decimal.Parse(Console.ReadLine());
+            string dateStr = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(dateStr, "d/M/yyyy", null);
+            decimal daysInMonth = DateTime.DaysInMonth(date.Year, date.Month);
+
+            decimal capsulesCount = decimal.Parse(Console.ReadLine());
+
+            decimal price = (daysInMonth * capsulesCount) * pricePerCapsule;
+
+            Console.WriteLine($"The price for the coffee is: ${price:F2}");
+
+            return price;
+
+        }
+    }
+}
---
+++ Exams/E01.Exam/E01.Exam/P03.RageQuit/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace P03.RageQuit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
